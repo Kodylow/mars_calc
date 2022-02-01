4a4d6661c9b959841e2e85e58d961614ebe2794d
Kody Low <kodylow7@gmail.com>
2022-02-01 09:44:32 -0500

    finished calc

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
-    calculate_weight_on_mars(100.0);
+    println!("Enter your weight in kg: ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+    println!("Weight on earth: {}", weight);
+
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    50.0
+    (weight / 9.81) * 3.711
 }
